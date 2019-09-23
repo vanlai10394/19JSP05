888865e4f6746c7421164b76ec65b6ee31d23610
Van Lai <vanlai10394@gmail.com>
2019-09-23 20:05:44 +0700

    add new int the project

---
+++ JSP05/src/A/A.java
@@ -0,0 +1,13 @@
+package A;
+
+public class A {
+	private int a=1;
+	public int b=2;
+	protected int c=5;
+	int d=3;
+	/*sadsadsadsa
+	 * dsf
+	 * sfd
+	 */
+	
+}
---
+++ JSP05/src/A/B.java
@@ -0,0 +1,10 @@
+package A;
+
+public class B {
+	public static void main(String[] args) {
+		A t=new A();
+		System.out.println("b="+t.b);
+		System.out.println("c="+t.c);
+		System.out.println("d="+t.d);
+	}
+}
